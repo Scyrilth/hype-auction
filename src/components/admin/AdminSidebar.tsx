36d5b16ae0e89e6209a9011b6049f163e925fa5d
Scyrilth <ahsanyoosuf.1997@gmail.com>
2026-07-01 03:50:19 +0500

    fix admin sidebar height - extend background to bottom of screen

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -21,7 +21,7 @@ export default function AdminSidebar() {
   const { exitAdminMode } = useViewMode();
 
   return (
-    <aside className="hidden h-full w-60 shrink-0 self-stretch flex-col border-r border-border bg-surface px-4 py-5 md:flex lg:w-64 lg:px-5">
+    <aside className="admin-sidebar hidden w-60 shrink-0 flex-col border-r border-border bg-surface px-4 py-5 md:flex lg:w-64 lg:px-5">
       <Link
         href="/"
         onClick={() => exitAdminMode()}
